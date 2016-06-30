638a86da4717fc8db78bb04f56a06bfb0d162a98
nakanoyoshiki <n.yoshiki67@gmail.com>
2016-06-30 21:55:31 +0900

    add database of post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,11 +1,25 @@
 class PostController < ApplicationController
+  def index
+    @twitter ||= Post.all
+  end
+
   def tweet
     @twitter ||= MyTwitter.new
     @twitter.tag = "鳥貴族"
     @twitter.limit = "50"
     @twitter.tweet = @twitter.client.search("##{@twitter.tag}", lang: "ja", result_type: 'recent', count: 20).map do |tweet|
-      Post.create_from_twitter tweet
+      create_from_twitter tweet
+
+       # {
+       #     icon: tweet.user.profile_image_url,
+       #     text: tweet.id,
+       #     data: tweet.user.created_at
+       # }
     end
     render action: 'index'
+  end
+
+  def create_from_twitter tweet
+    Post.create(text: tweet.text, image: tweet.user.profile_image_url)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,2 @@
 class Post < ActiveRecord::Base
-  def create_from_twitter tweet
-    Post.create(text: tweet.text, image: tweet.user.profile_image_url)
-  end
 end
