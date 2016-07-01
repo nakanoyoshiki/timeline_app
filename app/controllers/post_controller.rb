10385b5c2cf1f008b4c170a859d65c73527f5241
nakanoyoshiki <n.yoshiki67@gmail.com>
2016-07-01 09:02:24 +0900

    add post column and add grunavi api

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,12 +1,19 @@
 class PostController < ApplicationController
+  require 'json'
+  require 'uri'
   def index
   end
 
   def new
     @twitter ||= MyTwitter.new
     @twitter.tweet = @twitter.client.search('鳥貴族', lang: "ja", result_type: 'recent', count: 20).map do |tweet|
-       create_from_twitter tweet
+      create_from_twitter tweet
     end
+
+
+    key = 'e9c5ed396a549bfdf2bb6fe8c3cc0d3d'
+    @restaurant_url ||= "http://api.gnavi.co.jp/PhotoSearchAPI/20150630/?keyid=#{key}&format=json&shop_name=鳥貴族&hit_per_page=30"
+    @restaurant ||= JSON.parse(Net::HTTP.get(URI.parse(URI.escape(@restaurant_url))))
     @post = Post.all
     render action: 'index'
   end
